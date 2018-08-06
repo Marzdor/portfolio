e24046f34c5c99a14d6316743b293f39bf55aa8a
Max Ingersoll <marzdor@gmail.com>
2018-08-06 16:03:11 -0700

    updated projects

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-window.onload = function () {
-    genPortfolio();
-}
+window.onload = function() {
+  genPortfolio();
+};
 
 function genPortfolio() {
-    const baseNames = ["calculator", "localWeatherApp", "pomodoroClock", "quoteApp", "tictactoe", "travel-site-tut", "twitchStreamList", "wikipediaViewer"];
+  const baseNames = [
+    "calculator",
+    "local-Weather-App",
+    "pomodoro-Clock",
+    "tic-tac-toe",
+    "travel-site-tut",
+    "twitch-Stream-List",
+    "wikipedia-Viewer",
+    "react-Drum-Machine",
+    "react-Markdown-Previewer",
+    "react-Quote-App"
+  ];
 
-    for (let i = 0; i < baseNames.length; i++) {
-        const markup = `
-      <a class="portfolio-image" href="https://marzdor.github.io/${baseNames[i]}" target="_blank">
+  for (let i = 0; i < baseNames.length; i++) {
+    const markup = `
+      <a class="portfolio-image" href="https://marzdor.github.io/${
+        baseNames[i]
+      }" target="_blank">
             <div class="project-tile">
-                <img src="images/${baseNames[i]}_small.png" srcset="images/${baseNames[i]}.png 1400w" alt="Thumbnail of ${baseNames[i]} website.">
-                <h4 class="caption">${baseNames[i]}</h4>
+                <img src="images/${baseNames[i]}_small.png" srcset="images/${
+      baseNames[i]
+    }.png 1400w" alt="Thumbnail of ${baseNames[i]} website.">
+                <h4 class="caption">${baseNames[i].replace(/-/g, " ")}</h4>
             </div>
         </a>
       `;
-        const target = document.querySelector("#projects");
-        target.insertAdjacentHTML("beforeend", markup);
-    }
+    const target = document.querySelector("#projects");
+    target.insertAdjacentHTML("beforeend", markup);
+  }
 }
