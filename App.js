2d9039afe95a6e3be9d5d895f291528395ad0cef
Max Ingersoll <marzdor@gmail.com>
2018-12-20 15:13:15 -0800

    added my first MERN stack site

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ window.onload = function() {
 function genPortfolio() {
   const baseNames = [
     "DnD-5E",
+    "MERN-Stack",
     "react-calculator",
     "react-Drum-Machine",
     "react-Markdown-Previewer",
@@ -21,6 +22,7 @@ function genPortfolio() {
 
   const linkNames = [
     "DnD_5E_Site",
+    "MERN-Stack",
     "React-calculator",
     "React-drumMachine",
     "React-markdownPreviewer",
@@ -36,18 +38,33 @@ function genPortfolio() {
   ];
 
   for (let i = 0; i < baseNames.length; i++) {
-    const markup = `
-      <a class="portfolio-image" href="https://marzdor.github.io/${
-        linkNames[i]
-      }" target="_blank">
-            <div class="project-tile">
-                <img src="images/${baseNames[i]}_small.png" srcset="images/${
-      baseNames[i]
-    }.png 1400w" alt="Thumbnail of ${baseNames[i]} website.">
-                <h4 class="caption">${baseNames[i].replace(/-/g, " ")}</h4>
-            </div>
-        </a>
-      `;
+    let markup;
+
+    if (baseNames[i] === "MERN-Stack") {
+      markup = `
+        <a class="portfolio-image" href="https://infinite-garden-94657.herokuapp.com/" target="_blank">
+              <div class="project-tile">
+                  <img src="images/${baseNames[i]}_small.png" srcset="images/${
+        baseNames[i]
+      }.png 1400w" alt="Thumbnail of ${baseNames[i]} website.">
+                  <h4 class="caption">${baseNames[i].replace(/-/g, " ")}</h4>
+              </div>
+          </a>
+        `;
+    } else {
+      markup = `
+        <a class="portfolio-image" href="https://marzdor.github.io/${
+          linkNames[i]
+        }" target="_blank">
+              <div class="project-tile">
+                  <img src="images/${baseNames[i]}_small.png" srcset="images/${
+        baseNames[i]
+      }.png 1400w" alt="Thumbnail of ${baseNames[i]} website.">
+                  <h4 class="caption">${baseNames[i].replace(/-/g, " ")}</h4>
+              </div>
+          </a>
+        `;
+    }
     const target = document.querySelector("#projects");
     target.insertAdjacentHTML("beforeend", markup);
   }
